2be95e512d10d994b6970c95c4928ec268a04523
Bannasorn Sukan <136978678+Bannasorn@users.noreply.github.com>
2023-08-16 23:59:54 +0700

    hw 5-4

--- Homework/HW05/hw05-4-compare-number.cpp
+++ Homework/HW05/hw05-4-compare-number.cpp
@@ -26,114 +26,25 @@
         Number1 equal Number2
 */
 #include <stdio.h>
-#include <iostream>
-using namespace std;
 int main()
 {
-    int num = 0 ;
-    printf("Please Enter your number : ") ;
-    scanf("%d", &num) ;
-    if (num > -1 && num < 31)
+    int a = 0, b = 0 ;
+    printf("Input[1] Input[2] : ");
+    scanf("%d %d", &a, &b);
+    if (a > b)
     {
-        switch (num)
-        {
-        case 0:
-            cout << "Zero" ;
-            break;
-        case 1:
-            cout << "One";
-            break;
-        case 2:
-            cout << "Two";
-            break;
-        case 3:
-            cout << "Three";
-            break;
-        case 4:
-            cout << "Four";
-            break;
-        case 5:
-            cout << "Five";
-            break;
-        case 6:
-            cout << "Six";
-            break;
-        case 7:
-            cout << "Seven";
-            break;
-        case 8:
-            cout << "Eight";
-            break;
-        case 9:
-            cout << "Nine";
-            break;
-        case 10:
-            cout << "Ten";
-            break;
-        case 11:
-            cout << "Eleven";
-            break;
-        case 12:
-            cout << "Twelve";
-            break;
-        case 13:
-            cout << "Thirteen";
-            break;
-        case 14:
-            cout << "Fourteen";
-            break;
-        case 15:
-            cout << "Fifteen";
-            break;
-        case 16:
-            cout << "Sixteen";
-            break;
-        case 17:
-            cout << "Seventeen";
-            break;
-        case 18:
-            cout << "Eighteen";
-            break;
-        case 19:
-            cout << "Nineteen";
-            break;
-        case 20:
-            cout << "Twenty";
-            break;
-        case 21:
-            cout << "Twenty One";
-            break;
-        case 22:
-            cout << "Twenty Two";
-            break;
-        case 23:
-            cout << "Twenty Three";
-            break;
-        case 24:
-            cout << "Twenty Four";
-            break;
-        case 25:
-            cout << "Twenty Five";
-            break;
-        case 26:
-            cout << "Twenty Six";
-            break;
-        case 27:
-            cout << "Twenty Seven";
-            break;
-        case 28:
-            cout << "Twenty Eight";
-            break;
-        case 29:
-            cout << "Twenty Nine";
-            break;
-        case 30:
-            cout << "Thirty";
-            break;
-        }
+        printf("Number1 greater than Number2");
     }
-    else{
-        printf("Error");
+    else if (b > a)
+    {
+        printf("Number1 lower than Number2");
     }
+    else if (a == b)
+    {
+        printf("Number1 equal Number2");
+    }
+    else printf("Error");
+    
+    
     return 0;
 }
